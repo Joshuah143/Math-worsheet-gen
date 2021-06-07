27488f8e47d3e3d4e882f6794ab963c0ab71ce72
Joshua <83801098+Joshuah143@users.noreply.github.com>
2021-06-06 23:50:39 -0600

    setup

--- main.py
+++ main.py
@@ -2,24 +2,26 @@ import reportlab
 from reportlab.pdfgen.canvas import Canvas
 import random
 import os
+import math
+import time
+
 
 def arithmetic_arranger(list_of_question=None, return_or_not=False, pdfable=False, genquestions=False):
     if list_of_question is None:
         list_of_question = []
-    for i in range(7):
-        top = random.randint(1000, 9999)
-        bottom = random.randint(1000, 9999)
-        # minus is 0 plus is 1
-        minus = random.randint(0, 1)
-        while (not minus) and bottom >= top:
-            bottom = random.randint(1, 9999)
-        if minus:
-            minus = '+'
-        else:
-            minus = '-'
-        list_of_question.append(str(str(top) + ' ' + minus + ' ' + str(bottom)))
-    print(list_of_question)
-
+    if genquestions:
+        for i in range(7):
+            top = random.randint(1000, 9999)
+            bottom = random.randint(1000, 9999)
+            # minus is 0 plus is 1
+            minus = random.randint(0, 1)
+            while (not minus) and bottom >= top:
+                bottom = random.randint(1, 9999)
+            if minus:
+                minus = '+'
+            else:
+                minus = '-'
+            list_of_question.append(str(str(top) + ' ' + minus + ' ' + str(bottom)))
     one = ''
     two = ''
     three = ''
@@ -60,15 +62,52 @@ def quadratic_aranger(modifyco=False, largefactors=False):
     # e = ac
     # f = bc + ad
     # g = bd
+    comidify = 7
+    factorranger = 10
+    factorincrease = 15
     if modifyco:
-        a, b = random.randint(1, 7), random.randint(1, 7)
+        a = random.randint(1, comidify)
+        c = random.randint(1, comidify)
     else:
-        a, b = 1
-    print(a, b)
-    pass
+        a, c = 1, 1
+    if largefactors:
+        factorranger += factorincrease
+    b, d = random.randint(1, factorranger), random.randint(1, factorranger)
+    e = int((a * c))
+    f = int((b * c) + (a * d))
+    g = int((b * d))
+    string = f'{e}x + {f}x + {g}'
+    solution = [a, b, c, d]
+    while (checkforfactors(solution) not in solution) and (checkforfactors(solution) != None):
+        factor = checkforfactors(solution)
+        solution.append(factor)
+        e = e / factor
+        f = f / factor
+        g = g / factor
+    solutions = f'({a}x+{b})({c}x+{d})'
+    for o in range(len(solution) - 4):
+        ting = o + 4
+        solutions += f'({solution[ting]})'
+    return [(string, solutions), e, f, g]
 
 
-if False:
+def checkforfactors(lists):
+    for i in range(2, max(lists) + 1):
+        t = 0
+        for v in lists:
+            if v % i == 0:
+                t += 1
+        if t == len(lists):
+            return i
+    return None
+
+
+def listfactors(num):
+    facts = lambda num: [i for i in range(2, num) if num % i == 0]
+    return facts(num)
+
+
+def printadditionsubtraction():
     canvas = Canvas("Teacher-addition.pdf")
     canvas2 = Canvas("Student-subtraction.pdf")
     cs = list(arithmetic_arranger(pdfable=True, genquestions=True))
@@ -99,4 +138,81 @@ if False:
     canvas2.save()
 
 
+def printfactor(problems=15, probsgetharder=False):
+    canvas = Canvas('Student-factor.pdf')
+    canvas2 = Canvas('Teacher-factor.pdf')
+    watermarktop = 'Math worksheet'
+    watermarkbttom = 'By Joshua Himmens, joshua.himmens@gmail.com'
+    instructions = 'Factor completly:'
+    canvas2.setFont('Courier', 20)
+    canvas.setFont('Courier', 20)
+    canvas.drawString(220, 770, watermarktop)
+    canvas2.drawString(220, 770, watermarktop)
+    canvas2.setFont('Courier', 12)
+    canvas.setFont('Courier', 12)
+    canvas.drawString(150, 750, watermarkbttom)
+    canvas2.drawString(150, 750, watermarkbttom)
+    canvas.drawString(228, 700, instructions)
+    canvas2.drawString(228, 700, instructions)
+    canvas2.setFont('Courier', 12)
+    canvas.setFont('Courier', 12)
+    top = 650
+    oldtop = top
+    leftjustify = 50
+    probspace = 40
+    qnum = 0
+    intervals = [10, 20]
+    for i in range(problems):
+        if qnum < intervals[0]:
+            prob = quadratic_aranger()
+        if intervals[0] < qnum < intervals[1]:
+            prob = quadratic_aranger(largefactors=True)
+        if qnum > intervals[1]:
+            prob = quadratic_aranger(modifyco=True, largefactors=True)
+        canvas.drawString(leftjustify, top, prob[0][0] + ':')
+        canvas2.drawString(leftjustify, top, prob[0][0] + ':')
+        canvas2.setFont('Courier', 5)
+        canvas.setFont('Courier', 5)
+        if prob[1] < 10:
+            canvas.drawString(leftjustify+15, top+5, '2')
+            canvas2.drawString(leftjustify+15, top+5, '2')
+        else:
+            canvas.drawString(leftjustify + 22, top + 5, '2')
+            canvas2.drawString(leftjustify + 22, top + 5, '2')
+        canvas2.setFont('Courier', 12)
+        canvas.setFont('Courier', 12)
+        canvas2.drawString(leftjustify + 120, top, prob[0][1])
+        top -= probspace
+        qnum += 1
+    leftjustify = 300
+    top = oldtop
+    for i in range(problems):
+        if qnum < intervals[0]:
+            prob = quadratic_aranger()
+        if intervals[0] < qnum < intervals[1]:
+            prob = quadratic_aranger(largefactors=True)
+        if qnum > intervals[1]:
+            prob = quadratic_aranger(modifyco=True, largefactors=True)
+        canvas.drawString(leftjustify, top, prob[0][0] + ':')
+        canvas2.drawString(leftjustify, top, prob[0][0] + ':')
+        canvas2.setFont('Courier', 5)
+        canvas.setFont('Courier', 5)
+        if prob[1] < 10:
+            canvas.drawString(leftjustify + 15, top + 5, '2')
+            canvas2.drawString(leftjustify + 15, top + 5, '2')
+        else:
+            canvas.drawString(leftjustify + 22, top + 5, '2')
+            canvas2.drawString(leftjustify + 22, top + 5, '2')
+        canvas2.setFont('Courier', 12)
+        canvas.setFont('Courier', 12)
+        canvas2.drawString(leftjustify + 120, top, prob[0][1])
+        top -= probspace
+        qnum += 1
+    canvas.save()
+    canvas2.save()
 
+
+time.sleep(2)
+printfactor(probsgetharder=True)
+printadditionsubtraction()
+print('done')
